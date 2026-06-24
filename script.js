18d7ca39a8f117cc8b846215f45a49964b9a9b8b
Adisha Hirushan <adishahirushan@gmail.com>
2026-06-24 22:23:46 +0530

    few ui updates

--- script.js
+++ script.js
@@ -292,23 +292,9 @@ window.addEventListener("load", () => {
 
     // Change button text
     if (document.body.classList.contains("dark-mode")) {
-        toggleBtn.textContent = "Switch to Light Mode";
+        toggleBtn.textContent = "🌞";
     } else {
-        toggleBtn.textContent = "Switch to Dark Mode";
-    }
-    });
-
-    const toggleBtn1 = document.getElementById("modeTogglemob");
-
-    toggleBtn1.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    document.body.classList.toggle("light-mode");
-
-    // Change button text
-    if (document.body.classList.contains("dark-mode")) {
-        toggleBtn1.textContent = "Switch to Light Mode";
-    } else {
-        toggleBtn1.textContent = "Switch to Dark Mode";
+        toggleBtn.textContent = "🌙";
     }
     });
 
